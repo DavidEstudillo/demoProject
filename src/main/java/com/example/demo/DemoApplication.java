986367611bee48d83b89d3cd299bf6664491f1a6
David <davidestudillo1994@gmail.com>
2022-01-05 10:18:26 +0100

    changed Date fields to LocalDateTime fields

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,25 +18,17 @@ public class DemoApplication {
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(DemoApplication.class, args);
 		PricesRepository pricesRepository = configurableApplicationContext.getBean(PricesRepository.class);
 		
-		SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");			
+		DateTimeFormatter formatter= DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");			
 		
-		try {
-			
-			Prices price1 = new Prices(1, formatter.parse("2020-06-14-00.00.00"), formatter.parse("2020-12-31-23.59.59"), 1, 35455, 0, 35.50, "EUR");
-			Prices price2 = new Prices(1, formatter.parse("2020-06-14-15.00.00"), formatter.parse("2020-06-14-18.30.00"), 2, 35455, 1, 25.45, "EUR");
-			Prices price3 = new Prices(1, formatter.parse("2020-06-15-00.00.00"), formatter.parse("2020-06-15-11.00.00"), 3, 35455, 1, 30.50, "EUR");
-			Prices price4 = new Prices(1, formatter.parse("2020-06-15-16.00.00"), formatter.parse("2020-12-31-23.59.59"), 4, 35455, 1, 38.95, "EUR");
-			
-			pricesRepository.save(price1);
-			pricesRepository.save(price2);
-			pricesRepository.save(price3);
-			pricesRepository.save(price4);
-			
-		} catch (ParseException e) {
-			
-			e.printStackTrace();
-			
-		}		
+		Prices price1 = new Prices(1, LocalDateTime.parse("2020-06-14-00.00.00", formatter), LocalDateTime.parse("2020-12-31-23.59.59", formatter), 1, 35455, 0, 35.50, "EUR");
+		Prices price2 = new Prices(1, LocalDateTime.parse("2020-06-14-15.00.00", formatter), LocalDateTime.parse("2020-06-14-18.30.00", formatter), 2, 35455, 1, 25.45, "EUR");
+		Prices price3 = new Prices(1, LocalDateTime.parse("2020-06-15-00.00.00", formatter), LocalDateTime.parse("2020-06-15-11.00.00", formatter), 3, 35455, 1, 30.50, "EUR");
+		Prices price4 = new Prices(1, LocalDateTime.parse("2020-06-15-16.00.00", formatter), LocalDateTime.parse("2020-12-31-23.59.59", formatter), 4, 35455, 1, 38.95, "EUR");
+		
+		pricesRepository.save(price1);
+		pricesRepository.save(price2);
+		pricesRepository.save(price3);
+		pricesRepository.save(price4);		
 		
 	}
 
